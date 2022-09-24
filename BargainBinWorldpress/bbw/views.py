ed69349f32dff9312b6da318e280a6a5af2a727d
Dmitry Stepanov <bugagolick@gmail.com>
2022-09-24 20:24:49 +0300

    Basic views

--- BargainBinWorldpress/bbw/views.py
+++ BargainBinWorldpress/bbw/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import *
 # Create your views here.
+
+
+class PostList(ListView):
+    model = Post
+    # ordering = '-publication_date'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+
+class PostDetails(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
